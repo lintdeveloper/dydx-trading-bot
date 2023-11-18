4f7de279a859712b4c5315ad23a66aae817cd4c2
lintdeveloper <musabrillz@gmail.com>
2023-11-18 15:49:09 +0100

    project constants added

--- program/main.py
+++ program/main.py
@@ -1,3 +1,11 @@
+from func_connections import connect_dydx
 
 if __name__ == '__main__':
-  print('Hello bot!')
+  
+  try:
+    print('Connecting to client...')
+    client = connect_dydx()
+  except Exception as e:
+    print('Error connecting to client: ', e)
+    # send_message(f'Failed to connect to client {e}')
+    exit(1)
